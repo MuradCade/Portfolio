ababcba98e2ee5d33bb4d04b5fc7e4b12beccd2e
MuradCade <king12murad@gmail.com>
2022-07-05 04:32:42 -0700

    portfolio is done

--- src/components/Home.js
+++ src/components/Home.js
@@ -14,8 +14,8 @@ import { Helmet } from 'react-helmet';
               <p className='uppercase text-sm tracking-widest text-gray-500'>
                   LET'S BUILD SOMETHING TOGETHER
               </p>
-              <h1 className='text-2xl font-medium py-2 text-gray-700'>Hi, I'm <span className='text-[#5651e5]'>MuradCade</span></h1>
-              <h1 className='font-medium text-xs   mt-1 text-gray-500'>
+              <h1 className='text-2xl font-medium py-1 text-gray-700'>Hi, I'm <span className='text-[#5651e5]'>MuradCade</span></h1>
+              <h1 className='font-medium text-xs  text-gray-500'>
                  Web & App Developer
               </h1>
               <p className='py-2 text-gray-700 max-w-[70%] m-auto font-medium'>I'm Software Developer specializing In Building (and occasionally designing) exceptional  digital experiences. Currently, I'm focused on building Web Application And Keeping Up With diferent Technologies.</p>
